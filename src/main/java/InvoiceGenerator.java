fe83b6da0d9f81bfd8265a6374bb3436f723bdbb
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-08-23 11:23:09 +0530

    Added:Calculate Fare

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,5 +1,20 @@
 public class InvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome To the Cab Invoice Generator");
+    /*
+     * Given : MINIMUM_COST_PER_KM = 10.0
+     *         COST_PER_TIME = 1
+     *         MINIMUM_FARE = 5
+     */
+    private static final double MINIMUM_COST_PER_KM = 10.0;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
+
+    /*
+     * Purpose : Given distance and time,
+     *           Return total fare for the journey
+     */
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
