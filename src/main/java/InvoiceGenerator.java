639ce732b602a9352fb80cdb2a5e95e3709de72a
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-08-23 11:27:48 +0530

    Added:Multiple Rides

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,8 +1,8 @@
 public class InvoiceGenerator {
     /*
      * Given : MINIMUM_COST_PER_KM = 10.0
-     *         COST_PER_TIME = 1
-     *         MINIMUM_FARE = 5
+     * COST_PER_TIME = 1
+     * MINIMUM_FARE = 5
      */
     private static final double MINIMUM_COST_PER_KM = 10.0;
     private static final int COST_PER_TIME = 1;
@@ -10,11 +10,25 @@ public class InvoiceGenerator {
 
     /*
      * Purpose : Given distance and time,
-     *           Return total fare for the journey
+     * Return total fare for the journey
+     * <p>
+     * Condition : If minimum total fare is less than the MINIMUM_FARE, return MINIMUM_FARE
      */
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
 
         return Math.max(totalFare, MINIMUM_FARE);
+    }
+
+    /*
+     * Purpose : Given distance and time for multiple rides,
+     * Return aggregate total fare for all the journey
+     */
+    public double calculateTotalFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
     }
 }
